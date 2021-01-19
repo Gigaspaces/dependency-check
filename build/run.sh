9d5cebad5b7e9e4b2b1508caa22b21f8b915ec9c
meshiruff <meshi.ruff@gigaspaces.com>
2021-01-19 17:06:57 +0200

    test M2

--- build/run.sh
+++ build/run.sh
@@ -10,7 +10,7 @@ function upload_artifact {
     local target="$2"
 
 
-    cmd="mvn -B -Dmaven.repo.local=/var/m2_dependency_check/repository com.gigaspaces:xap-build-plugin:deploy-native -Dput.source=${zipPath} -Dput.target=${target}"
+    cmd="mvn -B -P s3 -Dmaven.repo.local=/var/m2_dependency_check/repository com.gigaspaces:xap-build-plugin:deploy-native -Dput.source=${zipPath} -Dput.target=${target}"
 
     echo "****************************************************************************************************"
     echo "uploading $1"

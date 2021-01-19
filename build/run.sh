a518c206e181b45ef1a0d6d8274dba281134d529
meshiruff <meshi.ruff@gigaspaces.com>
2021-01-19 12:01:40 +0200

    run check

--- build/run.sh
+++ build/run.sh
@@ -26,21 +26,23 @@ function upload_artifact {
 }
 
 GS_VERSION=$1
-##wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.0.5/dependency-check-6.0.5-release.zip
-#unzip dependency-check-6.0.5-release.zip
-#
-##wget https://gigaspaces-releases-eu.s3.amazonaws.com/xap/16.0.0/gigaspaces-xap-enterprise-${GS_VERSION}.zip
-#unzip gigaspaces-xap-enterprise-16.0.0-m1-ci-7.zip
-#
-#cd dependency-check/bin
-#./dependency-check.sh --project "xap-${GS_VERSION}" --scan "../../gigaspaces-xap-enterprise-16.0.0-m1-ci-7" --out .
-
 echo "test"
 echo ${GS_VERSION}
 echo $M2
-DEPENDENCY_BUCKET="dependency-check-results"
+
+##wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.0.5/dependency-check-6.0.5-release.zip
+unzip dependency-check-6.0.5-release.zip
+wget https://gigaspaces-releases-eu.s3.amazonaws.com/xap/16.0.0/gigaspaces-xap-enterprise-${GS_VERSION}.zip
+unzip gigaspaces-xap-enterprise-${GS_VERSION}.zip
+#
 mkdir -p ${GS_VERSION}
-cp dependency-check-report.html ${GS_VERSION}/
-upload_artifact ${GS_VERSION}/ ${DEPENDENCY_BUCKET}
+cd dependency-check/bin
+
+./dependency-check.sh --project "xap-${GS_VERSION}" --scan "../../gigaspaces-xap-enterprise-${GS_VERSION}" --out ${GS_VERSION}/
+
+DEPENDENCY_BUCKET="dependency-check-results"
+
+#cp dependency-check-report.html ${GS_VERSION}/
+#upload_artifact ${GS_VERSION}/ ${DEPENDENCY_BUCKET}
 
 rm -r ${GS_VERSION}

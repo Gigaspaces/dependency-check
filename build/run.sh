17f2b0f7d1d503c04f1ac38db25b82ecc63a9bed
Meshi Ruff <46296187+meshiruff@users.noreply.github.com>
2021-01-19 11:30:38 +0200

    Update run.sh
    upload to s3

--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env sh
 set -x
 
+
+function upload_artifact {
+# $1 = zipPath
+# $2 = target
+    echo "uploading zip $1 to "
+    local zipPath="$1"
+    local target="$2"
+
+
+    cmd="mvn -B -P s3 -Dmaven.repo.local=$M2/repository com.gigaspaces:xap-build-plugin:deploy-native -Dput.source=${zipPath} -Dput.target=${target}"
+
+    echo "****************************************************************************************************"
+    echo "uploading $1"
+    echo "Executing cmd: $cmd"
+    echo "****************************************************************************************************"
+    eval "$cmd"
+    local r="$?"
+    if [ "$r" -eq 1 ]
+    then
+        echo "[ERROR] failed to upload $1 , exit code:$r"
+        exit 1
+    fi
+}
+
 GS_VERSION=$1
 ##wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.0.5/dependency-check-6.0.5-release.zip
 #unzip dependency-check-6.0.5-release.zip
@@ -13,3 +37,10 @@ GS_VERSION=$1
 
 echo "test"
 echo ${GS_VERSION}
+echo $M2
+DEPENDENCY_BUCKET="dependency-check-results"
+mkdir -p ${GS_VERSION}
+cp dependency-check-report.html {GS_VERSION}/
+upload_artifact {GS_VERSION}/ ${DEPENDENCY_BUCKET}
+
+rm -r ${GS_VERSION}

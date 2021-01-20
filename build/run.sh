f9af8aef0776762194d3f8a2908517bbe7df74e3
meshiruff <meshi.ruff@gigaspaces.com>
2021-01-20 11:06:56 +0200

    dependency check folder

--- build/run.sh
+++ build/run.sh
@@ -4,7 +4,7 @@ function uploadToS3 {
     echo "uploading html $1 to "
     local zipPath="$1"
     local target="$2"
-    cmd="mvn -B -P dependency-check -Dmaven.repo.local=/home/jenkins/.m2_dependency_check/repository com.gigaspaces:xap-build-plugin:deploy-native -Dput.source=${zipPath} -Dput.target=${target}"
+    cmd="mvn -B -P dependency-check -Dmaven.repo.local=${M2}/repository com.gigaspaces:xap-build-plugin:deploy-native -Dput.source=${zipPath} -Dput.target=${target}"
     echo "Executing cmd: $cmd"
     eval "$cmd"
     local r="$?"
@@ -18,8 +18,16 @@ function uploadToS3 {
 
 GS_VERSION=$1
 GS_URL=$2
-##wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.0.5/dependency-check-6.0.5-release.zip
-#unzip dependency-check-6.0.5-release.zip
+
+if [ ! -e "dependency-check-6.0.5-release" ]
+then
+    if [ ! -e "dependency-check-6.0.5-release.zip" ]
+    then
+      wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.0.5/dependency-check-6.0.5-release.zip
+    fi
+	unzip dependency-check-6.0.5-release.zip
+fi
+
 rm -r gigaspaces-*
 wget ${GS_URL}
 unzip gigaspaces-*.zip
@@ -31,9 +39,10 @@ mkdir -p ${GS_VERSION}
 cd dependency-check/bin
 ./dependency-check.sh --project "xap-${GS_VERSION}" --scan "../../${FOLDER_NAME}" --out ${GS_VERSION}/
 
-echo ${WORKSPACE}
-cd /var/workspaces/Metric/Spotinst/Dependency-Check
+cd ${WORKSPACE}
 DEPENDENCY_BUCKET="dependency-check-results"
 uploadToS3 /var/workspaces/Metric/Spotinst/Dependency-Check/build/dependency-check/bin/${GS_VERSION}/ ${DEPENDENCY_BUCKET}
-rm -r /var/workspaces/Metric/Spotinst/Dependency-Check/build/dependency-check/bin/${GS_VERSION}
-rm -r gigaspaces-*
+
+
+rm -r ${WORKSPACE}/build/dependency-check/bin/${GS_VERSION}
+rm -r ${WORKSPACE}/gigaspaces-*
